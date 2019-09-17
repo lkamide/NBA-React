17ae202bba12b6880f7bcd78aa6291923a7522d7
lkamide <lkamide@br.ibm.com>
2019-09-17 14:15:39 -0300

    e2e test on travis  -- headless

--- test.js
+++ test.js
@@ -1,9 +1,22 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
- 
+let chrome = require('selenium-webdriver/chrome');
 (async function example() {
+  //   const chromeOptions = new chrome.Options();
+  // chromeOptions.addArguments("--blink-settings=imagesEnabled=false");
+
+  var chromeOptions = new chrome.Options();
+    chromeOptions.addArguments("test-type");
+    chromeOptions.addArguments("start-maximized");
+    chromeOptions.addArguments("--js-flags=--expose-gc");
+    chromeOptions.addArguments("--enable-precise-memory-info");
+    chromeOptions.addArguments("--disable-popup-blocking");
+    chromeOptions.addArguments("--disable-default-apps");
+    chromeOptions.addArguments("--disable-infobars");
+    chromeOptions.addArguments("--headless");
+
   let driver = await new Builder()
   .forBrowser('chrome')
-  .setChromeOptions('--headless')
+  .setChromeOptions(chromeOptions)
   .build();
   
   try {
