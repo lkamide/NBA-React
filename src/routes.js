37b4a7beed3edfbc9d07135c58c05ba77a6caff2
lkamide <lkamide@br.ibm.com>
2019-09-17 10:36:45 -0300

    setting up the e2e testing adding travis configs

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-
-
 import Header from './components/header';
 import Footer from './components/footer';
 import Home from './components/Home/index';
 
+import Teams from './components/Teams'
+import Team from './components/Teams/team'
 
 const Routes = () => (
     <BrowserRouter>
         <Header/>
             <Switch>
+            <Route path="/teams/:id" component={Team} />
+                <Route path="/teams" component={Teams} />
                 <Route path="/" component={Home} />
             </Switch>
        <Footer/>
